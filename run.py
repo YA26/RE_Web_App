2c6f3454c846629674901ad6c738c8d3481191ea
unknown <yannvok2@gmail.com>
2019-12-18 14:56:54 +0100

    changes in rental_incidents_classification_model.h5, Model.py and run.py

--- run.py
+++ run.py
@@ -26,7 +26,7 @@ def submitText():
     rounded_probability =  None
     sentence            =  request.form["message"]
     if len(sentence.strip())!=0:
-        X_new = build_model.get_only_meaningful_sentences_without_label(tf_idf_dict, [sentence])
+        X_new = build_model.get_meaningful_sentences_only_without_label(tf_idf_dict, [sentence], 5)
         if len(X_new)==0:
         	label 		        = "❌ Texte non pertinent"
         	rounded_probability	= 0
@@ -34,15 +34,15 @@ def submitText():
     	    y_new_obs_predicted = build_model.predict(X_new, categories_classification_model, categories) 
     	    label               = y_new_obs_predicted[0][0] 
     	    probability         = y_new_obs_predicted[0][1]
-    	    rounded_probability = np.round(float(probability),3)
+    	    rounded_probability = np.round(float(probability), 3)
         return jsonify({'status':'OK', 'label':label, 'probability':rounded_probability})
     return jsonify({'status':'OK', 'warning':" ⚠️ Vous n'avez rien entré"})
 
-"""
+
 if __name__ == "__main__":
     app.run(debug=True)
     
-"""
+
 
 
 
